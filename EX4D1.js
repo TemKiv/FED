a40dec09def968a86f017de9c841c2a804b7adaf
Teemu <teemu.kivinen@edu.turkuamk.fi>
2023-03-17 07:48:15 +0200

    lisätty demoja

---
+++ EX4D1.js
@@ -0,0 +1,18 @@
+function isLeapYear(year){
+    try{
+        if (year=="") throw "Missing argument year error";
+        if (!Number.isInteger(year)) throw "Non-integer argument year error";
+    } catch (e){
+        console.log(e);
+    }
+
+    if((year%4==0)&& (year % 100 == 0)||(year % 400 == 0)){
+        console.log("Year "+year+" is a leap year");
+    } else {
+        console.log("Year "+year+" is not a leap year");
+    }
+}
+
+const year = Number(prompt("Enter a year: "));
+
+isLeapYear();
---
+++ function isLeapYear(year){.js
@@ -0,0 +1,17 @@
+function isLeapYear(year){
+    if((year%4==0)&& (year % 100 == 0)||(year % 400 == 0)){
+        console.log("Year "+year+" is a leap year");
+    } else {
+        console.log("Year "+year+" is not a leap year");
+    }
+}
+
+const year = Number(prompt("Enter a year: "));
+
+try{
+    isLeapYear();
+    if (year=="") throw "Missing argument year error";
+    if (!Number.isInteger(year)) throw "Non-integer argument year error";
+} catch (e){
+    console.log(e);
+}
