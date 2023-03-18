c1486399e014a067367dd791018162d8b1b36566
Teemu <teemu.kivinen@edu.turkuamk.fi>
2023-03-18 02:36:59 +0200

    korjattu demo1 ja demo2

--- function isLeapYear(year){.js
+++ function isLeapYear(year){.js
@@ -8,10 +8,14 @@ function isLeapYear(year){
 
 const year = Number(prompt("Enter a year: "));
 
-try{
-    isLeapYear();
-    if (year=="") throw "Missing argument year error";
-    if (!Number.isInteger(year)) throw "Non-integer argument year error";
-} catch (e){
-    console.log(e);
+function tryIsLeapYear(){
+    try{
+        if (year=="") throw "Missing argument year error";
+        if (!Number.isInteger(year)) throw "Non-integer argument year error";
+    } catch (e){
+        console.log(e);
+    }
 }
+
+tryIsLeapYear()
+isLeapYear()
